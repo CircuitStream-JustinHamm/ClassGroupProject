050f361b8fd812c20816390a5bccc3dd3066d6cf
Justin Hamm <Justin.T.Hamm@gmail.com>
2026-07-30 19:36:36 -0700

    Added hello world to MergeDemo

--- Assets/Scripts/MergeDemo.cs
+++ Assets/Scripts/MergeDemo.cs
@@ -8,6 +8,11 @@ public class MergeDemo : MonoBehaviour
         
     }
 
+    void HelloWorld()
+    {
+        Debug.Log("Hello, world!");
+    }
+
     // Update is called once per frame
     void Update()
     {
